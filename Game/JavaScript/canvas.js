46e3999541c92053bce8fac9ec8f60744b0582a3
John-v77 <john.vs11@hotmail.com>
2022-01-22 11:05:42 -0500

    generates map - matrix

--- Game/JavaScript/canvas.js
+++ Game/JavaScript/canvas.js
@@ -8,13 +8,15 @@
     let terrain
 
 
-    //map size in px
+    //map 
+    let numTiles = 16
+        // size in px
     let tileSize = 32
-    let mapSize = tileSize * 16
+    let mapSize = tileSize * numTiles
 
     /*  world map - list to be use for generating obstacles 
         and use for path finding Algos                  */
-    world = []
+    worldMap = []
 
 
 
@@ -36,6 +38,20 @@ const onload=()=>{
     ctx.fillStyle="F0F0F0"
 
 
+    //draw bolder tile
+    // let img1 = new Image()
+    // img1.src = '../Images/bolder-tile.png'
+    // img1.onload = function(){
+    //     ctx.drawImage(img1, 0, 0)
+    // }
+
+    //draw grass tile
+    // let img2 = new Image()
+    // img2.src = 'https://64.media.tumblr.com/5310eb96570ee4e51acae3ae0f57fd2e/9e54f617e5091267-f4/s540x810/cac009e7c086af567dc76a7690b8c4731d3b70d9.png'
+    // img2.onload = function(){
+    //     ctx.drawImage(img2, 32, 32)
+    // }
+
 
     //draw grid
 
@@ -53,8 +69,56 @@ const onload=()=>{
         ctx.stroke()
     }
     
-
+    createWorld()
     
 }
 
 
+// Game Logic ****************************************************
+
+
+// #1
+function createWorld(){
+
+    console.log('creating world')
+
+    createsMapList(numTiles)
+    
+    console.log(worldMap)
+}
+
+
+
+
+// #2
+function createsMapList(numOfTiles){
+
+    console.log('creating createsMapList')
+
+    //creates a 2d matrix that hold information about every tile
+
+    let val
+    
+    for(let x=0; x<numOfTiles; x++){
+
+        //creates a list to hold the row data
+        worldMap[x] = []
+        for(let y=0; y<numOfTiles; y++){
+
+            //polulates each list, 0=flat terrain, 1=impasable terrain
+            val = createObstacles()
+            worldMap[x][y] = val
+        }
+    }
+}
+
+// #3
+function createObstacles(){
+
+    /* random obstacles - can generate more or less if modifying "numOb"
+     smaller the number more obstacle you get*/
+
+    let numOb = 0.75
+    if(Math.random() > numOb) return 1
+    return 0
+}
